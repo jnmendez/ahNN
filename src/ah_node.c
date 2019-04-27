9a532ccad9d2d622a5ef1574866411cf728cab21
nicolas <jnmendez@gmail.com>
2019-04-27 19:11:18 +0200

    First commit ahNN

---
+++ src/ah_node.c
@@ -0,0 +1,11 @@
+/*
+ * ah_node.c
+ *
+ *  Created on: 27 abr. 2019
+ *      Author: nicolas
+ */
+
+void ah_node_init( void )
+{
+
+}
---
+++ src/ah_node.h
@@ -0,0 +1,4 @@
+#ifndef _ah_node_h_
+#define ah_node_h_
+
+#endif

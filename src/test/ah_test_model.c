8c07445873f51baa8a171b75e9ea54bc0f7c2104
nicolas <jnmendez@gmail.com>
2019-05-11 23:20:02 +0200

    Prediction extracted from Keras model

--- src/test/ah_test_model.c
+++ src/test/ah_test_model.c
@@ -50,20 +50,33 @@ void model_T1( void )
 void model_T2( void )
 {
 	int result ;
+	// IN [ 0.39232655 -1.02954193] OUT [0.519832   0.25513762]
+	// BIAS1 -0.183314, -0.209205
+	// BIAS2 -0.333418, -0.309407
+	// W2 (0,0): 0.125651, -0.331226,
+    //    (1,0): 0.664594, -1.11351
+	// W1 (0,0): 0.725246, 0.777699,
+    //    (1,0): -0.22734, 0.050874
+
 
 	ah_model_init( model );
 	ah_model_add_input_layer( model, 2, 2, &ah_sigmoid );
 
 	ah_model_add_layer(model, 2, &ah_sigmoid);
 
-	float input2[2] 	= {(float)0.07585818,  (float)0.006692851};
+	float input2[2] 	= {(float)0.39232655,  (float)-1.02954193};
+
+	float weight1[2] 	= {(float)0.377540669, (float)0.010986943};
 	float weight2[2] 	= {(float)0.377540669, (float)0.010986943};
-	float bias2[2] 		= {(float)0.924141820, (float)0.924141820};
+
+	float bias1[2] 		= {(float)-0.183314, (float)-0.209205};
+	float bias2[2] 		= {(float)-0.333418, (float)-0.309407};
+
 	float output2[2]	= {(float)0.0, (float)0.0};
 
-	ah_model_set_weight(model, 0, weight2);
+	ah_model_set_weight(model, 0, weight1);
 	ah_model_set_weight(model, 1, weight2);
-	ah_model_set_bias(model, 0, bias2);
+	ah_model_set_bias(model, 0, bias1);
 	ah_model_set_bias(model, 1, bias2);
 
 	ah_model_prediction( model, input2, output2 );
